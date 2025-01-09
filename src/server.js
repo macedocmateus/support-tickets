fc4d6700987aae7631fa86a0ce90524867c69b3d
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-01-09 14:46:49 -0300

    add middlewares

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import http from 'node:http';
+import { jsonHandler } from './middlewares/jsonHandler.js';
 
-function listener(request, response) {}
+async function listener(request, response) {
+    await jsonHandler(request, response);
+    console.log(request.body);
+}
 
 http.createServer(listener).listen(3333);
