72bc7f178f07adb2bc446b1d135744bf02c26119
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-01-11 15:57:01 -0300

    add update tickets

--- src/middlewares/routesHandler.js
+++ src/middlewares/routesHandler.js
@@ -12,8 +12,9 @@ export function routesHandler(request, response) {
     if (route) {
         const routeParams = request.url.match(route.path);
 
-        const { query } = routeParams.groups;
+        const { query, ...params } = routeParams.groups;
 
+        request.params = params;
         request.query = query ? extractQueryParams(query) : {};
 
         return route.controller({ request, response, database });
